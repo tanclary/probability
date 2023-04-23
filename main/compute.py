fe25f1ede89672f5b7ffd0c0a9a6fa828365b103
Tanner Clary <tannerclary@Tanners-Mini.attlocal.net>
2023-04-23 00:14:09 -0700

    oops

--- main/compute.py
+++ main/compute.py
@@ -1,7 +1,7 @@
 def main():
     singleProb = input("What is the probability of the desired event?\n")
     chances = input("How many chances do you have?\n")
-    totalProb = (float(singleProb) ** float(chances)) * 100
+    totalProb = 100 - ((float(singleProb) ** float(chances)) * 100)
     print("There is a " + str(totalProb) + "% chance that event will occur at least once.")
 
 if __name__ == "__main__":
